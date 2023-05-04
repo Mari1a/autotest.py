02ff1b8298751d43c54acebff140cd72512ff88c
Mari1a <123752321+Mari1a@users.noreply.github.com>
2023-05-05 00:21:43 +0500

    Create  19.2.4_test.py
    Напишите по одному позитивному тесту для каждого метода калькулятора.

---
+++ 19.2.4_test.py
@@ -0,0 +1,26 @@
+import pytest
+from app.calc import Calculator
+
+
+class TestClac:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_abbing_cuccess(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division(self):
+        assert self.calc.division(self, 25, 5) == 5
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 6, 2) == 4
+
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self, 1, 0)
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
